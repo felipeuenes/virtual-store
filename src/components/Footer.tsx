99f4a39da3500e79c1eed6c9ee9433558be10c85
Joedson Ferreira <joedson.devel@gmail.com>
2023-02-07 09:59:56 -0300

    finish footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,38 +3,49 @@ import { FaInstagram, FaFacebookSquare } from "react-icons/fa";
 
 export function Footer() {
   return (
-    <footer className="w-full h-screen relative flex flex-col justify-around bg-footer bg-no-repeat bg-cover bg-center bg-fixed after:content-[''] after:absolute after:w-full after:h-full after:bg-[rgba(0 0 0 0.6)]">
-      <div className="max-w-[1100px] h-[200px] my-0 mx-auto py-0 px-4 z-[1] flex flex-col justify-between">
-        <div>
-          <h3>Entrar em Contato</h3>
-          <ul>
-            <li>
-              <a href="#">
-                <MdPhoneEnabled size={25} color="#fff" /> (88) 99635-2291
+    <footer className="w-full h-96 relative flex flex-col justify-around bg-footer bg-no-repeat bg-cover bg-center bg-fixed after:content-[''] after:absolute after:w-full after:h-full after:bg-[rgba(0 0 0 0.6)]">
+      <div className="w-2/4 h-[200px] my-0 mx-auto py-0 px-4 z-[1] flex flex-col gap-4">
+        <div className="flex flex-col items-center">
+          <h3 className="mb-4 font-bold text-base tracking-wide uppercase text-white">
+            Entrar em Contato
+          </h3>
+          <ul className="flex gap-3">
+            <li className="mx-0 mt-5 mb-3 inline-block">
+              <a href="#" className="flex text-white text-base relative gap-2">
+                <MdPhoneEnabled size={25} color="#fff" />
+                (88) 99635-2291
               </a>
             </li>
-            <li>
-              <a href="#">
+            <li className="mx-0 mt-5 mb-3 inline-block">
+              <a href="#" className="flex text-white text-base relative gap-2">
                 <MdEmail size={25} color="#fff" /> casademaria@rcc.com.br
               </a>
             </li>
-            <li>
-              <a href="#">
+            <li className="mx-0 mt-5 mb-3 inline-block">
+              <a href="#" className="flex text-white text-base relative gap-2">
                 <MdChat size={25} color="#fff" /> Chat
               </a>
             </li>
           </ul>
         </div>
-        <div>
-          <h3>Redes Sociais</h3>
-          <ul>
-            <li>
-              <a href="https://www.instagram.com/casademariaccr/">
+        <div className="flex flex-col items-center">
+          <h3 className="mb-4 font-bold text-base tracking-wide uppercase text-white">
+            Redes Sociais
+          </h3>
+          <ul className="flex gap-3">
+            <li className="mx-0 mt-5 mb-3 inline-block">
+              <a
+                href="https://www.instagram.com/casademariaccr/"
+                className="flex text-white text-base relative"
+              >
                 <FaInstagram size={25} color="#fff" />
               </a>
             </li>
-            <li>
-              <a href="https://www.facebook.com">
+            <li className="mx-0 mt-5 mb-3 inline-block">
+              <a
+                href="https://www.facebook.com"
+                className="flex text-white text-base relative"
+              >
                 <FaFacebookSquare size={25} color="#fff" />
               </a>
             </li>
@@ -46,7 +57,7 @@ export function Footer() {
         <small className="text-white">
           &copy; 2022. Todos os Direitos Reservados.
         </small>
-        <small className="text-white">
+        <small className="text-white flex gap-1">
           Desenvolvido por
           <a
             href="https://github.com/devjoedson91?tab=repositories"
